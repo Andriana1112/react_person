15440cda956efd98f8ebfe5bc2b95eba7e4b74af
Andriana Chepil <andriana1112.chepil@gmail.com>
2025-04-03 14:16:42 +0300

    add task solution3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,24 @@
 // export const Person = ({ person }) => ();
 import React from 'react';
 
-export const Person = ({ name, age, sex, isMarried, partnerName }) => (
-  <div className="Person">
-    <p className="Person__name">My name is {name}</p>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
 
-    {age && <p className="Person__age">I am {age}</p>}
-
-    <p className="Person__partner">
-      {isMarried
-        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </div>
-);
+  return (
+    <div className="Person">
+      <p className="Person__name">My name is {name}</p>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">
+        {isMarried ? (
+          <>
+            {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+          </>
+        ) : (
+          'I am not married'
+        )}
+      </p>
+    </div>
+  );
+};
 
 export default Person;
